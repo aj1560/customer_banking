e60d1b474d7372da7049505ab5047acafbbee150
Amar Joshi <aj2k1560@gmail.com>
2024-08-16 11:16:27 -0400

    updated cd_account.py, customer_banking.py, savings_account.py at 11:15 on 16th Aug

--- savings_account.py
+++ savings_account.py
@@ -18,14 +18,15 @@ def create_savings_account(balance, interest_rate, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
-    interest = 0
-    new_account = Account(balance, interest)
-
+    
+    new_account = Account(balance, 0)
+    #interest = 0 
+    #float(interest)
 
     # Calculate interest earned
      # ADD YOUR CODE HERE
 
-    new_interest_earned = balance * (apr/100 *months/12)
+    new_interest_earned = balance * (interest_rate/100 *months/12)
 
     print(new_interest_earned)
     # Update the savings account balance by adding the interest earned
@@ -34,9 +35,12 @@ def create_savings_account(balance, interest_rate, months):
     new_balance = balance + new_interest_earned
     # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
-    new_balance=new_account.set_balance()
+    Final_Bal=new_account.set_balance(new_balance)
+    print(Final_Bal)
     # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
- 
+    Final_Int_Earned=new_account.set_interest(new_interest_earned)
+    print(Final_Int_Earned)
     # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    return  Final_Bal, Final_Int_Earned
+           # ADD YOUR CODE HERE
